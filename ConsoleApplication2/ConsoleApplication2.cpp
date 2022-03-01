dd4ed8002547ad22881affb5babce3dba22d7a75
Матвей <@HOME-PC>
2022-03-01 21:55:11 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,24 @@
+﻿using namespace std;
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    double s,k,count,x, e;
+    cout << "Enter x: \n";
+    cin >> x;
+    cout << "Enter e: \n";
+    cin >> e;
+    count = 1;
+    s = 1 / pow(x, count);
+    k = s;
+    while (abs(s - (s - k) > e))
+    {
+        count++;
+        k = 1 / pow(x, count);
+        s += k;
+    } 
+    while (abs(s - (s - k) > e));
+    cout << "S(N): " << s << " N: " << count;
+}
+
